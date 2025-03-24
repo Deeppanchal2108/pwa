b4dd6d2698b05c9c13d8a18c9ece24552a65a826
Deeppanchal2108 <deepanchal2290@gmail.com>
2025-03-25 01:50:20 +0530

    Changes madee

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-/** @type {import('next').NextConfig} */
-import withPWA from "next-pwa";
+import type { NextConfig } from "next";
 
-const nextConfig = {
-  distDir: "build",
-  reactStrictMode: true,
-   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
